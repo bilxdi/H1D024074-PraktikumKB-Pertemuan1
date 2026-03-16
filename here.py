12e21c99bdeb7a9725116a7fb5d873aa2cd3e817
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2026-03-16 22:08:15 +0700

    don ig

--- here.py
+++ here.py
@@ -1,16 +1,31 @@
 import random, math
 
-print("== TEBAK ANGKA ==")
+print("TEBAK ANGKA")
 answer = random.randint(1,20)
 percobaan = 5
+riwayat = []
 
 while(percobaan != 0):
     tebak = int(input("\ntebak angka 1-20: "))
+    riwayat.append(tebak)
     percobaan = percobaan-1
+    jarak = math.fabs(tebak - answer)
+
     if (tebak == answer):
-        print("benar")
+        print("\ntebakan benar")
+        print("riwayat tebakan: ", riwayat)
         break
     else:
-        print(answer)
-        print("tidak benar, coba lagi")
-        print("sisa percobaan: ", percobaan)
+        if (percobaan == 0):
+            print("\npercobaan habis")
+            print("angka yang benar: ", answer)
+        else:
+            if (tebak < answer):
+                print("\nterlalu kecil")
+            else:
+                print("\nterlalu besar")
+            if (jarak <= 2):
+                print("sudah dekat")
+            print("\nsisa percobaan: ", percobaan)
+
+        print("riwayat tebakan: ", riwayat)
