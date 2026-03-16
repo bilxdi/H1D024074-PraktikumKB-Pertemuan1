8a0e6441692a0fe7bc258b0de2e88476a051095e
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2026-03-16 20:36:37 +0700

    3 comit

--- here.py
+++ here.py
@@ -1,1 +1,11 @@
 import random
+
+b = random.randint(1,10)
+a = 0
+while(a != b):
+    a = int(input("tebak angka 1-10: "))
+    if (a == b):
+        print("benar")
+        break
+    else:
+        print("tidak benar, coba lagi\n")
