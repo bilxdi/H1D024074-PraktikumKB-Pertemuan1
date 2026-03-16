9a471af6805ca1a3eadf350bc3369a8b28f4cf61
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2026-03-16 23:59:38 +0700

    tambah komen

--- aplikasisederhana.py
+++ aplikasisederhana.py
@@ -1,31 +1,31 @@
 import random, math
 
 print("TEBAK ANGKA")
-answer = random.randint(1,20)
-percobaan = 5
-riwayat = []
+answer = random.randint(1,20) #milih random jawaban antara 1-20
+percobaan = 5 #limit percobaan nebak 5 kali
+riwayat = [] #list buat contain riwayat tebakan
 
 while(percobaan != 0):
     tebak = int(input("\ntebak angka 1-20: "))
-    riwayat.append(tebak)
-    percobaan = percobaan-1
-    jarak = math.fabs(tebak - answer)
+    riwayat.append(tebak) #masukin input tebakan ke list riwayat
+    percobaan = percobaan - 1
+    jarak = math.fabs(tebak - answer) #hitung jarak tebakan sama jawaban
 
     if (tebak == answer):
         print("\ntebakan benar")
         print("riwayat tebakan: ", riwayat)
-        break
+        break #keluar dari loop jika sudah benar
     else:
         if (percobaan == 0):
             print("\npercobaan habis")
-            print("angka yang benar: ", answer)
+            print("angka yang benar: ", answer) #jika percobaan habis
         else:
             if (tebak < answer):
-                print("\nterlalu kecil")
+                print("\nterlalu kecil") #clue jika tebakan lebih kecil dari jawaban
             else:
-                print("\nterlalu besar")
+                print("\nterlalu besar") #clue jika tebakan lebih besar dari jawaban
             if (jarak <= 2):
-                print("sudah dekat")
+                print("sudah dekat") #clue jika tebakan mendekati dengan jawaban
             print("\nsisa percobaan: ", percobaan)
 
         print("riwayat tebakan: ", riwayat)
